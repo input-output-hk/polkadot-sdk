e428d700f61c0780cad56f876609f95b526bf4f0
michal <mpskowron@gmail.com>
2024-07-19 13:19:04 +0200

    IOG Partner Chains changes for AURA session management and runner

--- substrate/frame/grandpa/src/lib.rs
+++ substrate/frame/grandpa/src/lib.rs
@@ -564,7 +564,7 @@ impl<T: Config> sp_runtime::BoundToRuntimeAppPublic for Pallet<T> {
 
 impl<T: Config> OneSessionHandler<T::AccountId> for Pallet<T>
 where
-	T: pallet_session::Config,
+	T: sp_partner_chains_session::CurrentSessionIndex,
 {
 	type Key = AuthorityId;
 
@@ -618,7 +618,7 @@ where
 
 		// update the mapping to note that the current set corresponds to the
 		// latest equivalent session (i.e. now).
-		let session_index = <pallet_session::Pallet<T>>::current_index();
+		let session_index = <T as sp_partner_chains_session::CurrentSessionIndex>::current_session_index();
 		SetIdSession::<T>::insert(current_set_id, &session_index);
 	}
 
