3a9ecbb1618d06ba9a014f08b7b678962ceca966
Tomasz Bartos <tomasz.bartos@iohk.io>
2025-05-30 10:32:59 +0200

    Extended pallet_beefy Config with Hashing trait

--- substrate/bin/node/runtime/src/lib.rs
+++ substrate/bin/node/runtime/src/lib.rs
@@ -2900,6 +2900,7 @@ impl pallet_beefy::Config for Runtime {
 	type MaxSetIdSessionEntries = BeefySetIdSessionEntries;
 	type OnNewValidatorSet = MmrLeaf;
 	type AncestryHelper = MmrLeaf;
+	type Hashing = Keccak256;
 	type WeightInfo = ();
 	type KeyOwnerProof = sp_session::MembershipProof;
 	type EquivocationReportSystem =
